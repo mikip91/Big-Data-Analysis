1773bd5baa122305f4629b5c89679632d2913525
MikiP26 <miki.padhiary@talentica.com>
2019-03-20 10:41:59 -0400

    crawl

---
+++ Data-Collection-Scripts/ArticleCrawler.R
@@ -0,0 +1,28 @@
+install.packages('Rcrawler')
+install.packages("XML")
+library(XML)
+library(Rcrawler)
+# Reading articles
+CollectedArticles <- read.csv(file="D:/MS/2ndSem/DIC/Lab2/NewyorkTimes-Data/OldArticles.csv", header=TRUE, sep=",")
+CollectedArticles <- subset(CollectedArticles, select = -c(X))
+CollectedUrls = CollectedArticles$web_url
+
+content <- NULL
+for(i in c(1:nrow(CollectedArticles)))
+{
+  Data<-ContentScraper(Url = toString(CollectedUrls[i]),
+                       CssPatterns =c(".css-1i2y565"))
+  Data<- str_replace_all(Data, "[^[:alnum:]]", " ")
+  if(i>1){
+    content <- rbind(content, Data)
+  }else
+  {
+    content<- Data
+  }
+}
+content <- content[, 1]
+content <- data.frame(content)
+cols <- c("text")
+colnames(content) <- cols
+content <-data.frame(content)
+write.table(content$text, file = "D:/MS/2ndSem/DIC/Lab2/NewyorkTimes-Data/articles.txt", sep="\t", col.names = F, row.names = F, quote = F) 
