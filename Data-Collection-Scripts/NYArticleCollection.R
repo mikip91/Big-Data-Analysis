396e13b2903d3a4f175c95addd82bf1fbc6f2f38
MikiP26 <miki.padhiary@talentica.com>
2019-04-14 00:48:17 -0400

    removed stop_words

--- Data-Collection-Scripts/NYArticleCollection.R
+++ Data-Collection-Scripts/NYArticleCollection.R
@@ -4,22 +4,22 @@
 install.packages("rtimes")
 library(rtimes)
 Start_Date <- "20190401"
-End_Date <- "20190402"
+End_Date <- "20190404"
 
 # Setting up authentication 
 Sys.setenv(NYTIMES_AS_KEY = "q54vgEeqyyr2xPubZrlsK0PBkAyDzJN1")
 
 # Collecting articles
-NYDataframe <- as_search(q = "politics", begin_date = Start_Date, end_date = End_Date, all_results = TRUE)
+NYDataframe <- as_search(q = "elections", begin_date = Start_Date, end_date = End_Date, sleep=20, all_results = TRUE)
 NYDataframe <- NYDataframe$data
 NYDataframe <- subset(NYDataframe, select = -c(multimedia, keywords, byline.person))
 NYDataframe = NYDataframe[!duplicated(NYDataframe$`_id`),]
 NYDataframe = NYDataframe[!duplicated(NYDataframe$snippet),]
 NYDataframe = NYDataframe[!duplicated(NYDataframe$word_count),]
-write.csv(NYDataframe, file = "D:/MS/2ndSem/DIC/Lab2/newsData/Newpolitics.csv")
+write.csv(NYDataframe, file = "D:/MS/2ndSem/DIC/Lab2/newsData/elections.csv")
 
 # Reading Articles
-Articles <- read.csv(file="D:/MS/2ndSem/DIC/Lab2/newsData/politics.csv", header=TRUE, sep=",")
+Articles <- read.csv(file="D:/MS/2ndSem/DIC/Lab2/newsData/elections.csv", header=TRUE, sep=",")
 Articles <- subset(Articles, select = -c(X))
 names(Articles)[11] <- "_id"
 
@@ -28,6 +28,6 @@ Articles <- rbind(Articles,NYDataframe)
 Articles = Articles[!duplicated(Articles$`_id`),]
 Articles = Articles[!duplicated(Articles$snippet),]
 Articles = Articles[!duplicated(Articles$word_count),]
-write.csv(Articles, file = "D:/MS/2ndSem/DIC/Lab2/newsData/politics.csv")
+write.csv(Articles, file = "D:/MS/2ndSem/DIC/Lab2/newsData/elections.csv")
 
 
